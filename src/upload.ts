f5f5330c9b9dd9260db42132720a33aa34e5cb6e
Justin Hutchings <jhutchings1@users.noreply.github.com>
2020-01-20 22:08:27 -1000

    Revert "Use awaiter"
    This reverts commit 7d8534be38c2144095cfb7b45b6a3072eeac31a9.

--- src/upload.ts
+++ src/upload.ts
@@ -26,62 +26,60 @@ const io = __importStar(require("@actions/io"));
 const path = __importStar(require("path"));
 const fs = __importStar(require("fs"));
 const zlib_1 = __importDefault(require("zlib"));
-function run() {
-  return __awaiter(this, void 0, void 0, function* () {
-    try {
+async function run() {
+  try {
 
-        const sarifFolder = 'sarif';
-        io.mkdirP(sarifFolder);
+    const sarifFolder = 'sarif';
+    io.mkdirP(sarifFolder);
 
-        let sarif_data = ' ';
-        sarif_data = fs.readFileSync(process.env['INPUT_SARIFFILE'],'utf8');
+    let sarif_data = ' ';
+    sarif_data = fs.readFileSync(process.env['INPUT_SARIFFILE'],'utf8');
+    
+    const zipped_sarif = zlib_1.gzipSync(sarif_data).toString('base64');
 
-        const zipped_sarif = zlib_1.gzipSync(sarif_data).toString('base64');
+    const { GITHUB_TOKEN, GITHUB_REF } = process.env;
+    if (GITHUB_TOKEN && GITHUB_REF) {
+        const octokit = new github.GitHub(GITHUB_TOKEN);
 
-        const { GITHUB_TOKEN, GITHUB_REF } = process.env;
-        if (GITHUB_TOKEN && GITHUB_REF) {
-            const octokit = new github.GitHub(GITHUB_TOKEN);
+        const { data: checks } = await octokit.checks.listForRef(
+          {
+            ...github.context.repo,
+            ref: GITHUB_REF
+          });
 
-            const { data: checks } = await octokit.checks.listForRef(
-              {
-                ...github.context.repo,
-                ref: GITHUB_REF
-              });
+        const check_name = core.getInput('check_name');
 
-            const check_name = core.getInput('check_name');
+        let check_run_id;
+        if (check_name) {
+          check_run_id = checks.check_runs.filter(run => run.name === check_name)[0].id
+          // We're only interested in the check runs created from this action.
+          // This filters out only those check runs that share our check run name
+        } else {
+          check_run_id = checks.check_runs[0].id
+          // if check_name is not provided, fallback to naively using the latest check run
+        }
 
-            let check_run_id;
-            if (check_name) {
-              check_run_id = checks.check_runs.filter(run => run.name === check_name)[0].id
-              // We're only interested in the check runs created from this action.
-              // This filters out only those check runs that share our check run name
-            } else {
-              check_run_id = checks.check_runs[0].id
-              // if check_name is not provided, fallback to naively using the latest check run
-            }
+        console.log({
+         ...github.context.repo,
+         check_run_id,
+         output: {
+            title: 'SARIF alerts in a base64 zip',
+            summary: 'base64 zip',
+            text: zipped_sarif.length
+          }});
 
-            console.log({
-             ...github.context.repo,
-             check_run_id,
-             output: {
-                title: 'SARIF alerts in a base64 zip',
-                summary: 'base64 zip',
-                text: zipped_sarif.length
-              }});
-
-            await octokit.checks.update({
-             ...github.context.repo,
-             check_run_id,
-             output: {
-                title: 'SARIF alerts in a base64 zip',
-                summary: 'base64 zip',
-                text: zipped_sarif
-              }});
-          }
-      } catch (error) {
-        core.setFailed(error.message);
+        await octokit.checks.update({
+         ...github.context.repo,
+         check_run_id,
+         output: {
+            title: 'SARIF alerts in a base64 zip',
+            summary: 'base64 zip',
+            text: zipped_sarif
+          }});
       }
-  });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
